ab6317417c72e16d3cfb2e34f291d3e64daa1e44
joseor13 <oswirr13@mail.fresnostate.edu>
2019-02-24 00:31:27 -0800

    started angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'My-App',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MyFirstAngularApp';
+}
