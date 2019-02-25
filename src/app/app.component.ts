916923e0417e4cee548f6e39a57298dee4209381
joseor13 <oswirr13@mail.fresnostate.edu>
2019-02-25 01:13:33 -0800

    added scrits, add and delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyFirstAngularApp';
+  contacts = [];
+  constructor() {
+    this.contacts = [
+      {
+        ID: 1,
+        first_name: 'Jose',
+        last_name: 'Rosales',
+        email: 'joserosales@me.com'
+      },
+      {
+        ID: 2,
+        first_name: 'Oswi',
+        last_name: 'Gonzales',
+        email: 'joserosales@me.com'
+      },
+      {
+        ID: 3,
+        first_name: 'Leonel',
+        last_name: 'Messi',
+        email: 'joserosales@me.com'
+      }
+    ];
+  }
+  delete(e) {
+    console.log('from delete: ', e);
+    this.contacts.splice(e, 1);
+  }
+  addRow() {
+    this.contacts.push({
+      ID: 1,
+      first_name: 'Jose',
+      last_name: 'Rosales',
+      email: 'joserosales@me.com'
+    }
+    )
+  }
 }
